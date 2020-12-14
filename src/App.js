b842673e3cda52ed8b58581ab9ad4fa37ebb8e82
Jared Engler <jared.lite@gmail.com>
2020-12-14 12:00:56 -0600

    convert App to class component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
+
+import { Component } from 'react'
 import logo from './img/me-centered.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>jared.engler</h1>
-        <ul className="social-links">
-          <li>
-            <a className="social-link"
-                href="https://www.linkedin.com/in/jaredengler/"
-                target="_blank"
-                rel="noopener noreferrer">
-                linkedin
-            </a>
-          </li>
-          <li>
-            <a className="social-link"
-                href="https://www.github.com/lagooned"
-                target="_blank"
-                rel="noopener noreferrer">
-                github
-            </a>
-          </li>
-          <li>
-            <a className="social-link"
-                href='&#109;ailto&#58;%6A%61re%&#54;&#52;&#46;l&#105;%74%6&#53;&#64;gma&#37;6&#57;%6C&#46;c&#111;%6D'
-                target="_blank"
-                rel="noopener noreferrer">
-                email
-            </a>
-          </li>
-        </ul>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>jared.engler</h1>
+          <ul className="social-links">
+            <li>
+              <a className="social-link"
+                  href="https://www.linkedin.com/in/jaredengler/"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  linkedin
+              </a>
+            </li>
+            <li>
+              <a className="social-link"
+                  href="https://www.github.com/lagooned"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  github
+              </a>
+            </li>
+            <li>
+              <a className="social-link"
+                  href='&#109;ailto&#58;%6A%61re%&#54;&#52;&#46;l&#105;%74%6&#53;&#64;gma&#37;6&#57;%6C&#46;c&#111;%6D'
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  email
+              </a>
+            </li>
+          </ul>
+        </header>
+      </div>
+  }
+
 }
 
 export default App;
