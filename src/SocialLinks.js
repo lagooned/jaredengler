08eb2063df274a0dc7e12b012d2055a07fb134e3
Jared Engler <jared.lite@gmail.com>
2021-05-31 18:24:28 -0500

    update resume, make resume SocialLink href relative

--- src/SocialLinks.js
+++ src/SocialLinks.js
@@ -10,7 +10,7 @@ class SocialLinks extends Component {
       <SocialLink href="https://www.github.com/lagooned" label="github" />
       <SocialLink href="&#109;ailto&#58;%6A%61re%&#54;&#52;&#46;l&#105;%74%6&#53;&#64;gma&#37;6&#57;%6C&#46;c&#111;%6D" label="email" />
       <SocialLink href="https://lagooned.github.io/blog" label="blog" />
-      <SocialLink href="https://lagooned.github.io/jaredengler/resume.pdf" label="resume" />
+      <SocialLink href="/jaredengler/resume.pdf" label="resume" />
     </ul>
   }
 
